e634268ce3747a5f2918f8891e39167e5e0a050a
Trevor Nunes <trevor.nunes@gmail.com>
2012-01-22 21:01:09 -0500

    initial port

---
+++ README.md
@@ -0,0 +1,5 @@
+Port of 8bit home computer Atari 400 and related systems. 
+
+
+
+/froggyface tbotz.blogspot.com
---
+++ src/config.h
@@ -0,0 +1,48 @@
+/*
+ * config.h
+ *
+ *  Created on: 2012-01-17
+ *      Author: trevor nunes
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define STDC_HEADERS 1
+#define SOUND 1
+#define STEREO_SOUND 1
+#define SDL 1
+#define PACKAGE_VERSION "1.2.3.4"
+#define NTSC_FILTER 1
+#define SUPPORTS_CHANGE_VIDEOMODE 1
+//#define SUPPORTS_PLATFORM_CONFIGINIT 1
+#define SUPPORTS_PLATFORM_PALETTEUPDATE 1
+//#define SUPPORTS_PLATFORM_SLEEP
+#define XEP80_EM
+
+// generic 'ix stuff
+#define HAVE_FDOPEN 1
+#define HAVE_FFLUSH 1
+#define HAVE_GETCWD 1
+#define HAVE_INET_NTOA 1
+#define HAVE_SIGNAL_H 1
+
+#define HAVE_UNISTD_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STRNCPY 1
+#define HAVE_DIRENT_H 1
+#define HAVE_LIBZ 1
+#define HAVE_ERRNO_H 1
+#define HAVE_LIBPNG
+#define HAVE_TIME_H
+#define HAVE_MKDIR 1
+#define HAVE_CHMOD 1
+#define HAVE_STAT 1
+#define HAVE_FCNTL_H 1
+//#define HAVE_SIGNAL 1
+#define HAVE_SELECT 1
+#define HAVE_CLOCK 1
+#define TIME_WITH_SYS_TIME 1
+#define HAVE_USLEEP 1
+
+#endif /* CONFIG_H_ */
